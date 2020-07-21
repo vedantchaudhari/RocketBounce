c7a53c099fe9ad634890763ea28e3c936649c893
Zernab <zernabgohar@gmail.com>
2020-07-20 20:24:20 -0400

    Added javafx Signup/Signin/Welcome pages

--- RocketBounce/src/main/Main.java
+++ RocketBounce/src/main/Main.java
@@ -5,12 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("welcome.fxml"));
+        primaryStage.setTitle("Sign In");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
--- RocketBounce/src/module-info.java
+++ RocketBounce/src/module-info.java
@@ -1,7 +1,6 @@
 module RocketBounce {
     requires javafx.fxml;
     requires javafx.controls;
-    requires java.sql;
 
     opens main;
 }

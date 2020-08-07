dc19d4af924927d83ab451bf8b36c38bfd8b47f1
bo98117 <bopreme27@users.noreply.github.com>
2020-08-07 16:48:06 -0700

    Created ForgotPassword.fxml and ForgotPasswordController.java

---
+++ RocketBounce/src/main/ForgotPasswordController.java
@@ -0,0 +1,29 @@
+package main;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+
+
+public class ForgotPasswordController implements Initializable {
+
+    @FXML
+    private TextField textEmail;
+
+
+    @FXML
+    private void sendCode(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}

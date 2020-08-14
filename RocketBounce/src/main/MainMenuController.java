6a7559c159b1ff8d001231cb05a855769c3ebedf
bo98117 <bopreme27@users.noreply.github.com>
2020-08-14 14:29:59 -0700

    Created 'MainMenuController' and created method templates for all buttons.  Functionality of 'exit' button is implemented.

---
+++ RocketBounce/src/main/MainMenuController.java
@@ -0,0 +1,50 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainMenuController implements Initializable  {
+
+    @FXML
+    private void play(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void shop(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void setting(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void profile(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void customize(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void credits(ActionEvent event) {
+
+    }
+
+    @FXML
+    private void exit(ActionEvent event) {
+    System.exit(0);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
+}

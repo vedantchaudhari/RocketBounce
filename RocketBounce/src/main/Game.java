5b041c0be881361daf811aaff169ccc8e35b41bf
Vedant Chaudhari <vedantchaudhari@outlook.com>
2020-08-18 17:22:29 -0400

    Added  base game code for integration into javaFX

--- RocketBounce/src/main/Game.java
+++ RocketBounce/src/main/Game.java
@@ -1,4 +1,31 @@
 package main;
 
-public class Game {
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Stage;
+
+public class Game extends Application {
+    private Group root;
+
+    @Override
+    public void start(Stage gameStage) throws Exception {
+        gameStage.setTitle("Rocket Bounce");
+        gameStage.setResizable(false);
+
+        Parent root = initScene();
+        Scene main = new Scene(root, 1024, 768);
+        gameStage.setScene(main);
+        gameStage.show();
+    }
+
+    private Parent initScene() {
+        root = new Group();
+        Canvas canvas = new Canvas(1024, 768);
+
+        root.getChildren().addAll(canvas);
+        return root;
+    }
 }

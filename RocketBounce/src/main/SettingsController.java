b5aa33e14fe62591c641e8317745bf37ddcbb475
Zernab <zernabgohar@gmail.com>
2020-08-21 15:36:17 -0400

    New mainmenu screens

---
+++ RocketBounce/src/main/SettingsController.java
@@ -0,0 +1,29 @@
+package main;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SettingsController {
+    Stage stage = new Stage();
+    Scene scene;
+
+    @FXML
+    private void back(ActionEvent event) {
+        Node source = (Node) event.getSource();
+        stage = (Stage) source.getScene().getWindow();
+        stage.close();
+        try {
+            scene = new Scene(FXMLLoader.load(getClass().getResource("view/mainmenu.fxml")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        stage.setScene(scene);
+        stage.show();
+    }
+}
